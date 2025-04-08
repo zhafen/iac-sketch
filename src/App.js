3eb94fa18b34eae321317bf2abd724e9fd39578f
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-04-07 22:03:01 -0500

    Added the very start of the app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import * as d3 from "d3";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function MyButton() {
   return (
+    <button>I'm a button</button>
+  );
+}
+
+const resources = await d3.json("/resources.json");
+const workflow = await d3.json("/workflow.json");
+
+export default function App() {
+  return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Working.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
-export default App;
