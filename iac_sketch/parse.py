6fe0d490494cb652e72d6d2629c5b91040a9414b
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-04-19 16:34:04 -0500

    Implemented most of transform

--- iac_sketch/parse.py
+++ iac_sketch/parse.py
@@ -15,3 +15,49 @@ class Parser:
             with open(filename, 'r', encoding='utf-8') as file:
                 file_entities = yaml.safe_load(file)
             self.entities.update(file_entities)
+
+    def transform(self):
+        self.components = {
+            "entity": [],
+        }
+        for entity, comps in self.entities.items():
+            for i, entry in enumerate(comps):
+
+                forrmat_error = ValueError(
+                    f"Entity component {entity}.{i} is not formatted correctly."
+                )
+
+                # When just given a flag
+                if isinstance(entry, str):
+                    comp_entity = entry
+                    comp = {}
+                # When given values for a component
+                elif isinstance(entry, dict):
+                    # Check formatting
+                    if len(entry) != 1:
+                        raise forrmat_error
+                    comp_entity, comp = list(entry.items())[0]
+                # We should only have dictionaries or strings
+                else:
+                    raise forrmat_error
+
+                # Create the component
+                parsed = self.parse_component(comp_entity, comp)
+
+                self.components["entity"].append(
+                    {
+                        "entity": entity,
+                        "entity_ind": i,
+                        "component": comp_entity,
+                        "parsed": parsed,
+                    }
+                )
+
+    def parse_component(self, comp_entity: str, comp: dict) -> bool:
+        
+        # Look for the function to parse the entity
+        parse_fn = f"parse_component_{comp_entity}"
+        if not hasattr(self, parse_fn):
+            return False
+
+        return getattr(self, parse_fn)(comp)
