4cb6ca0cf1311f403d1a3073ec5f3680ffe90547
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-04-19 16:10:25 -0500

    Got extract working

--- iac_sketch/parse.py
+++ iac_sketch/parse.py
@@ -1,15 +1,17 @@
 import glob
-import os
 
 import yaml
 
+
 class Parser:
 
-    def __init__(self, dir: str):
-        self.dir = dir
+    def __init__(self, input_dir: str):
+        self.input_dir = input_dir
 
-    def load(self):
+    def extract(self):
 
         self.entities = {}
-        for filename in glob.glob("*.yaml"):
-            self.entities.upate(yaml.safe_load(filename))
+        for filename in glob.glob(f"{self.input_dir}/*.yaml"):
+            with open(filename, 'r', encoding='utf-8') as file:
+                file_entities = yaml.safe_load(file)
+            self.entities.update(file_entities)
