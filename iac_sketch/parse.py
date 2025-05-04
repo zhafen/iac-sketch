83ed2f60af278c17cb15b01170a81ba2d703231a
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-05-03 20:53:10 -0500

    Refactored Field and Entity out of parse.

--- iac_sketch/parse.py
+++ iac_sketch/parse.py
@@ -1,65 +1,9 @@
-from dataclasses import dataclass
 import glob
-import re
 
 import pandas as pd
 import yaml
 
-
-class Entity(str):
-    """Subclass of str to represent entities."""
-
-
-@dataclass
-class Field:
-    name: str
-    type: str
-    description: str = ""
-    multiplicity: str = "0..*"
-    default: str = ""
-
-    field_def_order = ["type", "multiplicity"]
-
-    @classmethod
-    def from_kv_pair(cls, field_key: str, field_value: str | dict[str, str]) -> "Field":
-
-        # Parse the overall field definition
-        # The awful regex expression is to match the field name and balance brackets
-        # It was spat out by copilot, but it works...
-        pattern = (
-            r"(?P<name>\w+)\s*"
-            + r"\[(?P<bracket>[^\[\]]*(?:\[[^\[\]]*\][^\[\]]*)*)]"
-            + r"(?:\s*=\s*(?P<default>.+))?"
-        )
-        match = re.match(pattern, field_key)
-
-        # Check results
-        if match is None:
-            raise ValueError(f"field key {field_key} is not formatted correctly.")
-        field_name = match.group("name")
-        if field_name is None:
-            raise ValueError(f"field key {field_key} is not formatted correctly.")
-        bracket_contents = match.group("bracket")
-
-        # Convert into keyword arguments
-        kwargs = {
-            "name": field_name,
-            "default": match.group("default"),
-        }
-        for i, field_attr_value in enumerate(bracket_contents.split("|")):
-            field_attr = cls.field_def_order[i]
-            kwargs[field_attr] = field_attr_value
-
-        if isinstance(field_value, str):
-            kwargs["description"] = field_value
-        elif isinstance(field_value, dict):
-            kwargs.update(field_value)
-        elif field_value is None:
-            pass
-        else:
-            raise ValueError(f"field key {field_key} is not formatted correctly.")
-
-        return cls(**kwargs)
+from .data import Field
 
 
 class Parser:
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,7 +1,5 @@
 import unittest
 
-import pandas as pd
-
 from iac_sketch import parse
 
 
@@ -25,64 +23,3 @@ class TestParser(unittest.TestCase):
         assert "component" in self.parser.comps
         assert "link" in self.parser.comps["component"]["entity"].values
         assert "data" not in self.parser.comps
-
-
-class TestField(unittest.TestCase):
-
-    def test_default(self):
-
-        field_key = "my_field [int]"
-        field_value = "This is my field"
-
-        field = parse.Field.from_kv_pair(field_key, field_value)
-        assert field.name == "my_field"
-        assert field.type == "int"
-        assert field.description == "This is my field"
-        assert field.multiplicity == "0..*"
-
-    def test_dict_provided(self):
-
-        field_key = "my_field [int]"
-        field_value = {
-            "description": "This is my field",
-            "multiplicity": "0..1",
-        }
-
-        field = parse.Field.from_kv_pair(field_key, field_value)
-        assert field.name == "my_field"
-        assert field.type == "int"
-        assert field.description == "This is my field"
-        assert field.multiplicity == "0..1"
-
-    def test_multiplicity_in_brackets(self):
-
-        field_key = "my_field [int|1..1]"
-        field_value = "This is my field"
-
-        field = parse.Field.from_kv_pair(field_key, field_value)
-        assert field.name == "my_field"
-        assert field.type == "int"
-        assert field.description == "This is my field"
-        assert field.multiplicity == "1..1"
-
-    def test_nested_brackets(self):
-
-        field_key = "my_field [dict[str, str]]"
-        field_value = "This is my field"
-
-        field = parse.Field.from_kv_pair(field_key, field_value)
-        assert field.name == "my_field"
-        assert field.type == "dict[str, str]"
-        assert field.description == "This is my field"
-        assert field.multiplicity == "0..*"
-
-    def test_default_provided(self):
-
-        field_key = "my_field [str] = 0..*"
-        field_value = "This is my field"
-
-        field = parse.Field.from_kv_pair(field_key, field_value)
-        assert field.name == "my_field"
-        assert field.type == "str"
-        assert field.description == "This is my field"
-        assert field.default == "0..*"
