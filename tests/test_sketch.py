732012884ee13a8fa93e45aadce6a06df0d757b5
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-07-06 17:45:16 -0500

    Minor fix to test_sketch

--- tests/test_sketch.py
+++ tests/test_sketch.py
@@ -6,8 +6,8 @@ from iac_sketch import sketch
 class TestArchitect(unittest.TestCase):
 
     def setUp(self):
-        self.test_data_dir = "./public/components"
-        self.architect = sketch.Architect(self.test_data_dir)
+        self.test_pattern = "./public/components/*.yaml"
+        self.architect = sketch.Architect(self.test_pattern)
 
     def test_etl(self):
         registry = self.architect.perform_registry_etl()
