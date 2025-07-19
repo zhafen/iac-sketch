96ac511f01fcd2e9e891d21860ecb7be8cdf5958
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-07-19 06:43:41 -0500

    Implemented test_designed

--- iac_sketch/system_tests.py
+++ iac_sketch/system_tests.py
@@ -4,17 +4,13 @@ from . import data
 
 def test_designed(registry: data.Registry) -> pd.DataFrame:
 
-    # Get the requirements and join with the entities that satisfy them
-    requirements = registry.view(["requirement", "description"])
-    satisfies = registry.view("satisfies")
-    requirements = requirements.merge(
-        satisfies,
+    # Get all requirements that are not satisfied by any component
+    return registry.view("requirement").reset_index().merge(
+        registry.view("link").query("link_type == 'satisfies'"),
         left_on="entity",
-        right_on="value",
+        right_on="target",
         how="left",
-    )
-
-    return requirements.query("satisfies.isna()")
+    ).query("source.isna()")
 
 def test_implemented(registry: data.Registry) -> pd.DataFrame:
 
