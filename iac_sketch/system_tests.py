43e0cfdcf013b2fcbd748a7bdc3c9a53e3375477
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-07-19 06:51:59 -0500

    Implemented test_implemented

--- iac_sketch/system_tests.py
+++ iac_sketch/system_tests.py
@@ -2,23 +2,36 @@ import pandas as pd
 
 from . import data
 
+
 def test_designed(registry: data.Registry) -> pd.DataFrame:
 
     # Get all requirements that are not satisfied by any component
-    return registry.view("requirement").reset_index().merge(
-        registry.view("link").query("link_type == 'satisfies'"),
-        left_on="entity",
-        right_on="target",
-        how="left",
-    ).query("source.isna()")
+    return (
+        registry.view("requirement")
+        .reset_index()
+        .merge(
+            registry.view("link").query("link_type == 'satisfies'"),
+            left_on="entity",
+            right_on="target",
+            how="left",
+        )
+        .query("source.isna()")
+    )
+
 
 def test_implemented(registry: data.Registry) -> pd.DataFrame:
 
-    assert False
+    # All requirements where the satisfies component status is not 'done'
+    reqs = registry.view("requirement").reset_index()
+    links = registry.view(["link", "status"]).query("`link.link_type` == 'satisfies'")
+    return reqs.merge(
+        links, left_on="entity", right_on="link.target", how="left"
+    ).query("`status.value` != 'done'")
 
 def test_defined(registry: data.Registry) -> pd.DataFrame:
 
     assert False
+
 
 def test_connected(registry: data.Registry) -> pd.DataFrame:
 
