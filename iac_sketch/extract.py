a549f05302cbc1a0a5eb65da109e27d1f42f4ce7
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-07-25 22:02:02 -0500

    Moved code into extract.py

---
+++ iac_sketch/extract.py
@@ -0,0 +1,61 @@
+import ast
+
+class ComponentVisitor(ast.NodeVisitor):
+
+    def __init__(self):
+        self.path = []
+        self.components = []
+        self.comp_count = 0
+
+    def generic_visit(self, node):
+
+        entity, comp_key = self.get_node_id(node)
+
+        # Create a component
+        comp = {}
+        self.comp_count = 0
+        for field_key, field_value in ast.iter_fields(node):
+            comp[field_key] = self.parse_field(field_value)
+
+        component = {
+            "entity": entity,
+            "comp_key": comp_key,
+            "component_type": node.__class__.__name__,
+            "component": comp,
+        }
+        self.components.append(component)
+
+        self.path.append(comp_key)
+        super().generic_visit(node)
+
+        self.path.pop()
+
+    def get_node_id(self, node):
+
+        # Get the path
+        if "name" in node._fields:
+            comp_key = node.name
+        else:
+            comp_key = str(self.comp_count)
+            self.comp_count += 1
+
+        entity = ".".join(self.path)
+
+        return entity, comp_key
+
+    def get_node_path(self, node):
+
+        entity, comp_key = self.get_node_id(node)
+        return f"{entity}.{comp_key}"
+
+    def parse_field(self, field_value):
+
+        if isinstance(field_value, list):
+            return [self.parse_field(item) for item in field_value]
+        if isinstance(field_value, dict):
+            return {key: self.parse_field(value) for key, value in field_value.items()}
+        if isinstance(field_value, ast.Constant):
+            return field_value.value
+        if isinstance(field_value, ast.AST):
+            return self.get_node_path(field_value)
+        raise ValueError(f"Unsupported field type: {type(field_value)}")
