48773a4b248665153c5dadfaa80c84a75315f2d4
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-08-24 16:24:02 -0500

    Cleaned up logic for test_implemented and test_designed.

--- iac_sketch/system_tests.py
+++ iac_sketch/system_tests.py
@@ -21,20 +21,18 @@ def test_designed(registry: data.Registry) -> pd.DataFrame:
     - satisfies: fully_designed
     """
 
-    # Get all requirements that are not satisfied by any component
-    return (
-        registry.view("requirement")
-        .reset_index()
-        .merge(
-            registry.view("link").query("link_type == 'satisfies'"),
-            left_on="entity",
-            right_on="target",
-            how="left",
-        )
-        .query("source.isna()")
-    )
+    # We start with the requirements that are not implemented
+    reqs = test_implemented(registry)
 
-def test_implemented(registry: data.Registry) -> pd.DataFrame:
+    # Then we just select those that don't even have a satisfies link yet
+    reqs = reqs.query("`link.link_type`.isna()")
+
+    return reqs
+
+def test_implemented(
+    registry: data.Registry,
+    allowed_statuses: list[str] = ["in production"],
+) -> pd.DataFrame:
     """
     Parameters
     ----------
@@ -44,20 +42,37 @@ def test_implemented(registry: data.Registry) -> pd.DataFrame:
     Returns
     -------
     pd.DataFrame
-        A DataFrame containing the test results.
+        A DataFrame containing the invalid entities.
 
     Components
     ----------
     - test
     - satisfies: fully_implemented
+    - todo: Add code that identifies parent nodes with unsatisfied child nodes.
     """
 
-    # All requirements where the satisfies component status is not 'done'
-    reqs = registry.view("requirement").reset_index()
-    links = registry.view(["link", "status"]).query("`link.link_type` == 'satisfies'")
-    return reqs.merge(
-        links, left_on="entity", right_on="link.target", how="left"
-    ).query("`status.value` != 'done'")
+    # Get entities with [requirement] components and any entities linked with
+    # a [satisfies]/[satisfied_by] or [parent]/[child] link.
+    # Entities without either have nan values, which will be among those returned.
+    reqs = (
+        registry.view("requirement")
+        .reset_index()
+        .merge(
+            registry.view(["link", "status"]).query(
+                "`link.link_type` in ['satisfies', 'parent']"
+            ),
+            left_on="entity",
+            right_on="link.target",
+            how="left",
+        )
+    )
+
+    # Drop the requirements that have children
+    reqs = reqs.query("`link.link_type` != 'parent'")
+    # Drop the requirements that have a valid status
+    reqs = reqs[~reqs["status.value"].isin(allowed_statuses)]
+
+    return reqs
 
 def test_defined(registry: data.Registry) -> pd.DataFrame:
     """
@@ -80,6 +95,7 @@ def test_defined(registry: data.Registry) -> pd.DataFrame:
     # All compdefs that are invalid
     return registry.view("compdef").query("~is_valid")
 
+
 def test_connected(registry: data.Registry) -> pd.DataFrame:
     """
     Parameters
@@ -99,6 +115,7 @@ def test_connected(registry: data.Registry) -> pd.DataFrame:
     """
 
     return registry.view("node").query("connected_component_group != 0")
+
 
 def test_no_forbidden_components(
     registry: data.Registry,
@@ -122,6 +139,7 @@ def test_no_forbidden_components(
     ----------
     - test
     - satisfies: no_forbidden_components
+    - todo: Add priority cutoff.
     """
 
     compinst = registry.view("compinst")
